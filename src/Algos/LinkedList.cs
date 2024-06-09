9a57dc6e225504f4144f88965c1db204508c77c3
Joe Bell <35937399+llebj@users.noreply.github.com>
2024-06-09 14:08:43 +0100

    Implemented AddToTail on LinkedList.

--- src/Algos/LinkedList.cs
+++ src/Algos/LinkedList.cs
@@ -5,6 +5,7 @@ namespace Algos;
 public class LinkedList<T> : IEnumerable<T>
 {
     private Node<T>? _head;
+    private Node<T>? _tail;
 
     public LinkedList()
     {
@@ -13,27 +14,9 @@ public class LinkedList<T> : IEnumerable<T>
 
     public LinkedList(IEnumerable<T> values)
     {
-        if (!values.Any())
-        {
-            return;
-        }
-
-        Node<T>? previous = null;
-
         foreach (var value in values)
         {
-            var node = new Node<T>(value);
-
-            if (previous is null)
-            {
-                previous = node;
-                _head = node;
-            }
-            else
-            {
-                previous.Next = node;
-                previous = node;
-            }
+            AddToTail(value);
         }
     }
 
@@ -46,8 +29,33 @@ public class LinkedList<T> : IEnumerable<T>
             node.Next = _head;
             _head.Previous = node;
         }
+        else
+        {
+            // If head is null then this is the first item to be added to the list,
+            // therefore, we need to set tail as well.
+            _tail = node;
+        }
 
         _head = node;
+    }
+
+    public void AddToTail(T value)
+    {
+        var node = new Node<T>(value);
+
+        if (_tail is not null)
+        {
+            _tail.Next = node;
+            node.Previous = _tail;
+        }
+        else
+        {
+            // If tail is null then this is the first item to be added to the list,
+            // therefore, we need to set head as well.
+            _head = node;
+        }
+
+        _tail = node;
     }
 
     public IEnumerator<T> GetEnumerator()
--- src/AlgosTests/LinkedListTests.cs
+++ src/AlgosTests/LinkedListTests.cs
@@ -41,4 +41,59 @@ public class LinkedListTests
         // Assert
         Assert.Equal("0,1,2,3", string.Join(',', linkedList));
     }
+
+    [Fact]
+    public void GivenAnEmptyLinkedList_WhenAnItemIsAddedToTail_ThenCorrectlyUpdateTheLinkedList()
+    {
+        // Arrange
+        var linkedList = new Algos.LinkedList<int>();
+
+        // Act
+        linkedList.AddToTail(0);
+
+        // Assert
+        Assert.Equal("0", string.Join(',', linkedList));
+    }
+
+    [Fact]
+    public void GivenAnExistingLinkedList_WhenAnItemIsAddedToTail_ThenCorrectlyUpdateTheLinkedList()
+    {
+        // Arrange
+        var values = new int[] { 1, 2, 3 };
+        var linkedList = new Algos.LinkedList<int>(values);
+
+        // Act
+        linkedList.AddToTail(4);
+
+        // Assert
+        Assert.Equal("1,2,3,4", string.Join(',', linkedList));
+    }
+
+    [Fact]
+    public void GivenAnEmptyLinkedList_WhenAnItemIsAddedToHead_ThenASubsequentItemCanBeAddedToTail()
+    {
+        // Arrange
+        var linkedList = new Algos.LinkedList<int>();
+
+        // Act
+        linkedList.AddToHead(0);
+        linkedList.AddToTail(1);
+
+        // Assert
+        Assert.Equal("0,1", string.Join(',', linkedList));
+    }
+
+    [Fact]
+    public void GivenAnEmptyLinkedList_WhenAnItemIsAddedToTail_ThenASubsequentItemCanBeAddedToHead()
+    {
+        // Arrange
+        var linkedList = new Algos.LinkedList<int>();
+
+        // Act
+        linkedList.AddToTail(0);
+        linkedList.AddToHead(1);
+
+        // Assert
+        Assert.Equal("1,0", string.Join(',', linkedList));
+    }
 }
