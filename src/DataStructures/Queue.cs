b1d7904e4e0edcf34e0f905817ffe7998c6528de
Joe Bell <35937399+llebj@users.noreply.github.com>
2024-06-15 14:50:45 +0100

    Implemented Queue using a LinkedList.

---
+++ src/DataStructures/Queue.cs
@@ -0,0 +1,33 @@
+﻿namespace DataStructures;
+
+public class Queue<T>
+{
+    private readonly LinkedList<T> _list = new();
+
+    public bool IsEmpty => _list.IsEmpty;
+
+    public T Dequeue()
+    {
+        if (_list.IsEmpty)
+        {
+            throw new InvalidOperationException("Unable to dequeue value; the queue is empty.");
+        }
+
+        var item = _list.GetHead();
+        _list.RemoveFromHead();
+
+        return item;
+    }
+
+    public void Enqueue(T item) => _list.AddToTail(item);
+
+    public T Peek()
+    {
+        if (_list.IsEmpty)
+        {
+            throw new InvalidOperationException("Unable to peek at first value; the queue is empty.");
+        }
+
+        return _list.GetHead();
+    }
+}
---
+++ src/DataStructuresTests/QueueTests.cs
@@ -0,0 +1,100 @@
+﻿namespace DataStructuresTests;
+
+public class QueueTests
+{
+    [Fact]
+    public void GivenAnEmptyQueue_WhenPeek_ThenThrowAnInvalidOperationException()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+
+        // Act
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => queue.Peek());
+    }
+
+    [Fact]
+    public void GivenAnEmptyQueue_WhenAValueIsEnqueued_ThenTheQueueIsNotEmpty()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+
+        // Act
+        queue.Enqueue(1);
+
+        // Assert
+        Assert.False(queue.IsEmpty);
+    }
+
+    [Fact]
+    public void GivenAQueueWithAnItem_WhenPeek_ThenReturnTheFirstValueAndTheQueueIsNotEmpty()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+        queue.Enqueue(1);
+
+        // Act
+        var result = queue.Peek();
+
+        // Assert
+        Assert.Equal(1, result);
+        Assert.False(queue.IsEmpty);
+    }
+
+    [Fact]
+    public void GivenAQueueWithMultipleItems_WhenPeek_ThenReturnTheFirstValue()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+
+        // Act
+        var result = queue.Peek();
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void GivenAnEmptyQueue_WhenAnItemIsDequeued_ThenThrowAnInvalidOperationException()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+
+        // Act
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+    }
+
+    [Fact]
+    public void GivenAQueueWithAnItem_WhenTheItemIsDequeued_ThenReturnTheCorrectValueAndTheQueueIsEmpty()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+        queue.Enqueue(1);
+
+        // Act
+        var result = queue.Dequeue();
+
+        // Assert
+        Assert.Equal(1, result);
+        Assert.True(queue.IsEmpty);
+    }
+
+    [Fact]
+    public void GivenAQueueWithMultipleItems_WhenAnItemIsDequeued_ThenReturnTheCorrectValueAndTheQueueIsNotEmpty()
+    {
+        // Arrange
+        var queue = new DataStructures.Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+
+        // Act
+        var result = queue.Dequeue();
+
+        // Assert
+        Assert.Equal(1, result);
+        Assert.False(queue.IsEmpty);
+    }
+}
